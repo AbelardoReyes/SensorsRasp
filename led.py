d74d1ce1b7c6599d4987daf00ae4086373c69ba8
Abelardo <abelardoreyes256@gmail.com>
2023-02-27 11:39:18 -0600

    Siu

--- led.py
+++ led.py
@@ -6,7 +6,6 @@ class Led:
     def __init__(self, pin):
         GPIO.setmode(GPIO.BOARD)
         self.pin = pin
-        GPIO.setup(self.pin, GPIO.OUT)
 
     def on(self):
         GPIO.output(self.pin, True)
