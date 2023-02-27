abb4e339ce0793767de817af29361023a9ec621b
Abelardo <abelardoreyes256@gmail.com>
2023-02-27 11:28:45 -0600

    Siu

--- led.py
+++ led.py
@@ -5,6 +5,7 @@ import time
 class Led:
     def __init__(self, pin):
         GPIO.setmode(GPIO.BOARD)
+        GPIO.setwarnings(False)
         self.pin = pin
         GPIO.setup(self.pin, GPIO.OUT)
 
