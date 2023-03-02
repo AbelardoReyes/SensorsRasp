1a522947973b3d3ccd0082c4952900bfde541f72
Abelardo <abelardoreyes256@gmail.com>
2023-03-02 14:33:27 -0600

    Commit

--- led.py
+++ led.py
@@ -1,18 +1,24 @@
 import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
-
-class Led:
+class Led: 
     def __init__(self, pin):
-        GPIO.setmode(GPIO.BOARD)
         self.pin = pin
-
-    def on(self):
-        GPIO.output(self.pin, True)
-
-    def off(self):
-        GPIO.output(self.pin, False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.OUT)
 
     def blink(self):
-            self.on()
-            input("Presione cualquier tecla para continuar...")
+        self.on()
+        time.sleep(1)
+        self.off()
+        time.sleep(1)
+
+    def on(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+
+    def off(self):
+        GPIO.output(self.pin, GPIO.LOW)
+
+    def __del__(self):
+        GPIO.cleanup()
