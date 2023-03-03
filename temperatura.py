7e561fe068e2e55ee91c8e7eebb12a03f3ddbea3
Crism00 <107272848+Crism00@users.noreply.github.com>
2023-03-03 10:06:56 -0600

    Temperature

--- temperatura.py
+++ temperatura.py
@@ -1,14 +1,27 @@
 import Adafruit_DHT
+import RPi.GPIO as GPIO
 
 class Temperatura:
     def __init__(self):
         self.sensor = Adafruit_DHT.DHT22
         self.pin = 4
+        self.relay_pin = 17
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.relay_pin, GPIO.OUT)
+        GPIO.output(self.relay_pin, GPIO.HIGH)
 
-    def get_temperatura(self):
-        temperatura = Adafruit_DHT.read_retry(self.sensor, self.pin)
-        return temperatura
+    def get_temperatura_humedad(self):
+        temperatura, humedad = Adafruit_DHT.read_retry(self.sensor, self.pin)
+        if temperatura is not None and humedad is not None:
+            return temperatura, humedad
+        else:
+            return None
 
-    def get_humedad(self):
-        humedad = Adafruit_DHT.read_retry(self.sensor, self.pin)
-        return humedad
+    def encender_relay(self):
+        GPIO.output(self.relay_pin, GPIO.LOW)
+
+    def apagar_relay(self):
+        GPIO.output(self.relay_pin, GPIO.HIGH)
+
+    def __del__(self):
+        GPIO.cleanup()
