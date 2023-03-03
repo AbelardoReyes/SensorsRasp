8a99054ba99691607f6b21a4e86dbf8a47215c48
Abelardo <abelardoreyes256@gmail.com>
2023-03-03 11:07:04 -0600

    funciones temperatura

--- temperatura.py
+++ temperatura.py
@@ -10,11 +10,8 @@ class Temperatura:
     def get_temperatura_humedad(self):
         temperatura, humedad = Adafruit_DHT.read(self.sensor, self.pin)
         if temperatura is not None and humedad is not None:
-
             datos = [temperatura, humedad]
             return datos
         else:
             return [0, 0]
 
-    def __del__(self):
-        GPIO.cleanup()
