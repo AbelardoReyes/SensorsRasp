46b7f674fc39a33827466b7562af508aa96c62fc
Abelardo <abelardoreyes256@gmail.com>
2023-03-03 11:05:58 -0600

    funciones temperatura

--- temperatura.py
+++ temperatura.py
@@ -5,25 +5,16 @@ class Temperatura:
     def __init__(self):
         self.sensor = Adafruit_DHT.DHT22
         self.pin = 4
-        self.relay_pin = 17
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.relay_pin, GPIO.OUT)
-        GPIO.output(self.relay_pin, GPIO.HIGH)
 
     def get_temperatura_humedad(self):
         temperatura, humedad = Adafruit_DHT.read(self.sensor, self.pin)
         if temperatura is not None and humedad is not None:
-            temperaturaC = (temperatura - 32) * 5/9
-            datos = [temperaturaC, humedad]
+
+            datos = [temperatura, humedad]
             return datos
         else:
             return [0, 0]
-
-    def encender_relay(self):
-        GPIO.output(self.relay_pin, GPIO.LOW)
-
-    def apagar_relay(self):
-        GPIO.output(self.relay_pin, GPIO.HIGH)
 
     def __del__(self):
         GPIO.cleanup()
