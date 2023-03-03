6aa836bff4ecc1a5fb29bb757155ace8e7d565fe
Abelardo <abelardoreyes256@gmail.com>
2023-03-03 11:22:13 -0600

    funciones led

--- sonido.py
+++ sonido.py
@@ -1,22 +1,13 @@
 import RPi.GPIO as GPIO
-import time
 
-
-class Led:
+class Sonido:
     def __init__(self, pin):
         self.pin = pin
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin, GPIO.OUT)
 
-    def blink(self):
-        self.on()
-        time.sleep(1)
-        self.off()
-        time.sleep(1)
-
     def on(self):
         GPIO.output(self.pin, GPIO.HIGH)
-
 
     def off(self):
         GPIO.output(self.pin, GPIO.LOW)
