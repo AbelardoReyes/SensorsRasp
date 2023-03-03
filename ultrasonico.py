baa4b6867d7504261fa0569d49f7b24dda7a79e1
Abelardo <abelardoreyes256@gmail.com>
2023-03-02 18:50:29 -0600

    funciones distancia

--- ultrasonico.py
+++ ultrasonico.py
@@ -1,6 +1,6 @@
 import time
 import RPi.GPIO as GPIO
-
+from pynput import keyboard as kb
 
 
 class Ultrasonico:
@@ -12,17 +12,18 @@ class Ultrasonico:
         GPIO.setup(self.echo, GPIO.IN)
 
     def medir(self):
-        GPIO.output(self.trigger, False)
-        time.sleep(0.00001)
-        GPIO.output(self.trigger, True)
+        while kb.is_pressed('q') == False:
+            GPIO.output(self.trigger, False)
+            time.sleep(0.00001)
+            GPIO.output(self.trigger, True)
 
-        while GPIO.input(self.echo) == False:
-            pulse_start = time.time()
-        while GPIO.input(self.echo) == True:
-            pulse_end = time.time()
-        sig_time = pulse_end - pulse_start
-        distance = sig_time / 0.000058
-        return distance
+            while GPIO.input(self.echo) == False:
+                pulse_start = time.time()
+            while GPIO.input(self.echo) == True:
+                pulse_end = time.time()
+            sig_time = pulse_end - pulse_start
+            distance = sig_time / 0.000058
+            return distance
 
     def __del__(self):
         GPIO.cleanup()
