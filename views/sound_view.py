d60567b6eb9b1352c07d69c8009563dc63e49ee5
caesarnetyet <caesarnetyet@gmail.com>
2023-03-06 10:54:01 -0600

    ya

--- views/sound_view.py
+++ views/sound_view.py
@@ -6,13 +6,17 @@ from sensores.sensor import Sensor
 class SoundView:
     def __init__(self, pin_in=4):
         self.sensor = Sensor(pin_in)
+        self.statusFalse = False
 
     def leer(self):
         print("Para detener la lectura pulsa Ctrl + C")
         try:
             while True:
-                print(self.sensor.read())
-                time.sleep(1)
+                if self.sensor.read():
+                    print("Sonido detectado")
+                else:
+                    print("...")
+
         except KeyboardInterrupt:
             print("Fin de la lectura")
             return
