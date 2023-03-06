d0aa9121f43dd24943f894504909d35144c1b9d8
caesarnetyet <caesarnetyet@gmail.com>
2023-03-06 10:36:21 -0600

    poyo

--- views/temp_view.py
+++ views/temp_view.py
@@ -9,7 +9,6 @@ class TempView:
 
     def temHum(self):
         print("Temperatura y humedad")
-        temperatura = self.sensor.get_temperatura_humedad()
-        datos = temperatura.get_temperatura_humedad()
+        datos = self.sensor.get_temperatura_humedad()
         print("Temperatura: ", datos[0], "C")
         print("Humedad: ", datos[1], "%")
